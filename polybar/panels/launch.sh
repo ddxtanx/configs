4fbcdbbf170b7d93965236c5082c268c234420a1
Garrett Credi <garrettcredi@gmail.com>
2023-04-11 17:29:28 -0500

    Nah we don't stan sway

--- polybar/panels/launch.sh
+++ polybar/panels/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/dracula"
+DIR="$HOME/.config/polybar/panels"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -11,4 +11,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q bottom -c "$DIR"/config.ini &
+polybar -q main -c "$DIR"/config.ini &
