a51d27f27097fd0f3641795d0e07382cc5204af8
Garrett Credi <garrettcredi@gmail.com>
2023-05-11 01:40:50 -0500

    identify wallpaper vs lock mpvpaper

--- mpvpaper/randomize_wallpaper.sh
+++ mpvpaper/randomize_wallpaper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 NEXT_WALLPAPER=$(~/.config/generate_wallpaper.sh)
-MPV_PIDS=$(pgrep -f "mpvpaper -vv") #-vv is important, as it makes sure only wallpaper processes are kill
+MPV_PIDS=$(grep -sa CMD=WALLPAPER /proc/*/environ | cut -d '/' -f 3) #-vv is important, as it makes sure only wallpaper processes are kill
 #I also run mpvpaper for the lockscreen, and those commands are not called with -vv
 NEXT_PID=$!
-COMMAND=$($HOME/.config/wallpaper_display_command.sh $NEXT_WALLPAPER)
+COMMAND="CMD=WALLPAPER $($HOME/.config/wallpaper_display_command.sh $NEXT_WALLPAPER -- wallpaper)"
 echo $COMMAND
 eval $COMMAND
 kill $MPV_PIDS
