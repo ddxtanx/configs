3075f46d622cddf879c01c3c0bc4e1ce1bd2b858
Garrett Credi <garrettcredi@gmail.com>
2023-06-09 15:50:00 -0500

    Better lockscreening

--- sway/generate_lockscreen.sh
+++ sway/generate_lockscreen.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-swayidle -w \
-  timeout 15 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on"' &
 
 WALLPAPER_DIR=$HOME/Desktop/Wallpapers/Default/
 WALLPAPER=$(~/.config/generate_wallpaper.sh)
 
 WALLPAPER_COMMAND=$($HOME/.config/wallpaper_display_command.sh $WALLPAPER)
 SWAYLOCKS=$(pgrep swaylock-plugin)
+NUM_SWAYLOCKS=$(echo $SWAYLOCKS | wc -w)
 LOCK_COMMAND="$GL_FLAGS swaylock-plugin -d --command \
   '$WALLPAPER_COMMAND' \
   >> ~/.local/state/swaylock-plugin.log 2>&1"
 
-echo $LOCK_COMMAND
-eval $LOCK_COMMAND
-
-pkill $SWAYLOCKS -SIGUSR1
-pkill $SWAYLOCKS -SIGKILL
-pkill --newest swayidle
+if [ $NUM_SWAYLOCKS -eq 0 ]; then
+  echo $LOCK_COMMAND
+  swayidle -w \
+    timeout 15 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
+  SWAYIDLE_PID=$!
+  eval $LOCK_COMMAND
+  kill $SWAYIDLE_PID
+fi
 
---
+++ sway/test_lockscreen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./generate_lockscreen.sh &
+sleep 5
+./generate_lockscreen.sh &
