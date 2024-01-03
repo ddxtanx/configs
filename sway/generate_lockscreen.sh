aa105232a9032f4735fefb8e2ef9094f0a0a6cfa
Garrett Credi <garrettcredi@gmail.com>
2024-01-03 16:38:53 -0600

    Better lockscreening

--- sway/generate_lockscreen.sh
+++ sway/generate_lockscreen.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+exec 120>~/.local/state/swaylock-plugin.lock
+flock -n 120 || exit 1
 
+trap "rm -f ~/.local/state/swaylock-plugin.lock" EXIT
 
 WALLPAPER_DIR=$HOME/Desktop/Wallpapers/Default/
 WALLPAPER=$(~/.config/generate_wallpaper.sh)
 
-swayidle -w \
-  timeout 15 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' &
+swayidle timeout 15 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' >> ~/.local/state/swaylock-plugin.log 2>&1 &
 SWAYIDLE_PID=$!
 
-WALLPAPER_COMMAND=$($HOME/.config/wallpaper_display_command.sh $WALLPAPER)
-LOCK_COMMAND="$GL_FLAGS swaylock-plugin -d -e --command \
-  '$WALLPAPER_COMMAND' \
-  >> ~/.local/state/swaylock-plugin.log 2>&1 && trap 'kill -9 $SWAYIDLE_PID' EXIT"
-
-exec 120>~/.local/state/swaylock-plugin.lock
-flock -n 120 || exit 1
-trap "rm -f ~/.local/state/swaylock-plugin.lock" EXIT
-
-echo $LOCK_COMMAND
-eval $LOCK_COMMAND
+$HOME/.config/sway/create_lockscreen.sh "$WALLPAPER" $SWAYIDLE_PID >> ~/.local/state/swaylock-plugin.log 2>&1
