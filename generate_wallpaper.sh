57f09027bf975e227d1fb79d54d1dd468b4d036b
Garrett Credi <garrettcredi@gmail.com>
2024-01-25 12:53:44 -0600

    update config

--- generate_wallpaper.sh
+++ generate_wallpaper.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-RANDOM_WALLPAPER=
 WALLPAPER_FOLDER=~/Desktop/Wallpapers/Default/
 NEXT_WALLPAPER=$(find $WALLPAPER_FOLDER -maxdepth 1 -iregex ".*\.\(jpg\|jpeg\|png\|gif\|mp4\)" -printf "%p\n" | shuf | head -1)
 
--- sway/generate_lockscreen.sh
+++ sway/generate_lockscreen.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+echo "kill swayidle"
+pkill -f "swayidle -w timeout 600"
+echo "killed swayidle"
+
+echo "launching new swayidle"
+swayidle timeout 15 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' >> ~/.local/state/swaylock-plugin.log 2>&1 &
+SWAYIDLE_PID=$!
+echo "swayidle launched"
+
 echo "obtaining lock"
 exec 120>~/.local/state/swaylock-plugin.lock
 flock -n 120 || exit 1
 echo "lock obtained"cho 
 
 trap "rm -f ~/.local/state/swaylock-plugin.lock" EXIT
-echo "kill swayidle"
-pkill -f swayidle
-echo "killed swayidle"
 
 WALLPAPER_DIR=$HOME/Desktop/Wallpapers/Default/
 WALLPAPER=$(~/.config/generate_wallpaper.sh)
 
-echo "launching new swayidle"
-swayidle timeout 15 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' >> ~/.local/state/swaylock-plugin.log 2>&1 &
-SWAYIDLE_PID=$!
-echo "swayidle launched"
 
 echo "creating lockscreen"
 $HOME/.config/sway/create_lockscreen.sh "$WALLPAPER" $SWAYIDLE_PID >> ~/.local/state/swaylock-plugin.log 2>&1
