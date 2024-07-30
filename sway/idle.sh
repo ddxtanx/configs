3612a920d602df71eed8ea83a2d30a618517e909
Garrett Credi <garrettcredi@gmail.com>
2024-07-29 22:35:44 -0500

    submoduling update and idle time decrease

--- sway/idle.sh
+++ sway/idle.sh
@@ -2,7 +2,7 @@
 mkdir ~/.local/state/idle.lock || exit 1
 echo "$(date) - idle.sh started" >> ~/.local/state/idle.log
 swayidle -w \
-         timeout 600 'daemonize $HOME/.config/sway/generate_lockscreen.sh' \
+         timeout 300 'daemonize $HOME/.config/sway/generate_lockscreen.sh' \
          before-sleep 'daemonize $HOME/.config/sway/generate_lockscreen.sh' >> ~/.local/state/idle.log 2>&1 
 trap "rm -rf ~/.local/state/idle.lock" EXIT
 rmdir ~/.local/state/idle.lock
