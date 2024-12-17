9620258db76a14f21ee9cefaf373bdb00a45edd9
Garrett Credi <garrettcredi@gmail.com>
2024-12-17 01:56:55 -0600

    new idle and change to notif in waybar

--- sway/idle.sh
+++ sway/idle.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-mkdir ~/.local/state/idle.lock || exit 1
-echo "$(date) - idle.sh started" >> ~/.local/state/idle.log
+echo "$(date) - idle.sh called" >> /home/ddxtanx/.local/state/idle.log
+mkdir /home/ddxtanx/.local/state/idle.lock || exit 1
+echo "$(date) - idle.sh started" >> /home/ddxtanx/.local/state/idle.log
 swayidle -w \
          timeout 300 'daemonize $HOME/.config/sway/generate_lockscreen.sh' \
-         before-sleep 'daemonize $HOME/.config/sway/generate_lockscreen.sh' >> ~/.local/state/idle.log 2>&1 
-trap "rm -rf ~/.local/state/idle.lock" EXIT
-rmdir ~/.local/state/idle.lock
+         before-sleep 'daemonize $HOME/.config/sway/generate_lockscreen.sh' >> /home/ddxtanx/.local/state/idle.log 2>&1 
+trap "rm -rf /home/ddxtanx/.local/state/idle.lock" EXIT
+rmdir /home/ddxtanx/.local/state/idle.lock
