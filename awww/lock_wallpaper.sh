1edc846547ea5e5ec543468d7c774445f11e360d
Garrett Credi (ddxtanx) <garrettcredi@gmail.com>
2026-08-31 04:38:00 -0400

    Wallpaper update

--- awww/lock_wallpaper.sh
+++ awww/lock_wallpaper.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # lock_wallpaper.sh
-rm /run/user/1000/wayland-0-awww-daemon.lock.sock
-$HOME/.local/bin/awww-daemon -n lock &
+export PATH="$HOME/.local/bin:$PATH"
+SOCKET="${XDG_RUNTIME_DIR:-/run/user/$(id - u)}/${WAYLAND_DISPLAY:-wayland-0}-awww-daemon.lock.sock"
+rm -f "$SOCKET"
+$HOME/.local/bin/awww-daemon -n lock --no-cache &
 DAEMON_PID=$!
-sleep 0.5
-$HOME/.local/bin/awww img -n lock -t random "$1" 
+for _ in $(seq 1 30); do
+    [ -S "$SOCKET" ] && break
+    sleep 0.1
+done
+if [ ! -S "$SOCKET" ]; then
+    echo "awww-daemon failed to start" >&2
+    kill "$DAEMON_PID" 2>/dev/null
+    exit 1
+fi
+$HOME/.local/bin/awww img -n lock --resize fit "$1" 
 wait "$DAEMON_PID"
--- sway/create_lockscreen.sh
+++ sway/create_lockscreen.sh
@@ -3,12 +3,12 @@
 #Called like this: create_lockscreen.sh [path]
 WALLPAPER=$1
 
-WALLPAPER_COMMAND="/home/ddxtanx/.config/awww/lock_wallpaper.sh $WALLPAPER"
+WALLPAPER_COMMAND="/home/ddxtanx/.config/awww/lock_wallpaper.sh \"$WALLPAPER\""
 echo "$(date) - $WALLPAPER_COMMAND" >> ~/.local/state/lockscreen.log
 
-PID1=$GL_FLAGS swaylock-plugin -e -l --color 00000000 --command "$WALLPAPER_COMMAND"
-PID2=$!
-echo "PID1: $PID1" >> ~/.local/state/lockscreen.log
+eval "$GL_FLAGS swaylock-plugin -e -l --color 00000000 --command \"$WALLPAPER_COMMAND\" >> ~/.local/state/lockscreen.log 2>&1"
+PID1=$!
+
 echo "PID2: $PID2" >> ~/.local/state/lockscreen.log
 echo "Done." >> ~/.local/state/lockscreen.log
 
