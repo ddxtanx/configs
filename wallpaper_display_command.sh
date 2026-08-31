7a7774a52463a3ea5dc25c38535c1e80fc757e5e
Garrett Credi <garrettcredi@gmail.com>
2026-08-31 03:40:47 -0500

    Wezterm and wallpaper update

--- wallpaper_display_command.sh
+++ wallpaper_display_command.sh
@@ -4,4 +4,4 @@ NEXT_WALLPAPER=$1
 # echo "$GL_FLAGS mpvpaper \
 #   -p -o \"loop profile=gpu-hq vo=gpu-next hwdec=auto-copy target-colorspace-hint=yes\" \
 #   \"$MONITOR\" $NEXT_WALLPAPER &"
-/home/ddxtanx/.local/bin/awww img $NEXT_WALLPAPER -t random --transition-duration 1
+awww img $NEXT_WALLPAPER -t random --transition-duration 1 --resize fit
